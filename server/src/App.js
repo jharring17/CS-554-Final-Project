55758aa1211470cc844bb4b5d1b1372c941d0f02
jrose0116 <jrose0116@gmail.com>
2023-11-09 23:00:53 -0500

    Removing unused imports

--- server/src/App.js
+++ server/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {Route, Link, Routes, useParams, useSearchParams, Navigate} from 'react-router-dom';
+import {Route, Routes} from 'react-router-dom';
 
 function App() {
   return (
