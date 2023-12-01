b386affead100e4c7e7b3de3ea662ff1534c4059
jrose0116 <jrose0116@gmail.com>
2023-11-30 21:24:43 -0500

    Add FriendsModal to PrivateRoutes

--- client/src/components/PrivateRoute.jsx
+++ client/src/components/PrivateRoute.jsx
@@ -1,13 +1,14 @@
 import {Navigate, Outlet} from 'react-router-dom';
 import React, {useContext} from 'react';
 import {AuthContext} from '../context/AuthContext';
+import FriendsModal from './FriendsModal';
 
 const PrivateRoute = () => {
   const {currentUser} = useContext(AuthContext);
   //console.log('Private Route Comp current user', currentUser);
   // If authorized, return an outlet that will render child elements
   // If not, return element that will navigate to login page
-  return currentUser ? <Outlet /> : <Navigate to='/signin' replace={true} />;
+  return currentUser ? <><Outlet /><FriendsModal /></> : <Navigate to='/signin' replace={true} />;
 };
 
 export default PrivateRoute;
