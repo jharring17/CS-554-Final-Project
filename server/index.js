a1501bfaf60ba893ebbbe279b1c0628c5b11e349
Isabella-Stone <ibellarose1@gmail.com>
2023-12-06 21:08:22 -0500

    add user to fb AND mongo via client

--- server/index.js
+++ server/index.js
@@ -1,22 +1,7 @@
-// // File to create an express server for the backend of the application.
-// import express from 'express';
-
-// const app = express();
-// const PORT = process.env.PORT || 3000;
-
-// // Define a default route
-// app.get('/', (req, res) => {
-// 	res.status(200).send('The backend server is active.');
-// });
-
-// // Start the server
-// app.listen(PORT, () => {
-// 	console.log(`Server is running on port ${PORT}`);
-// 	console.log(`http://localhost:${PORT}`);
-// });
-
 import express from 'express';
+import cors from 'cors';
 const app = express();
+app.use(cors());
 import configRoutes from './routes/index.js';
 
 app.use(express.json()); //***if you don't have this the request body will be undefined, this is what allows you to read the request body in a route
