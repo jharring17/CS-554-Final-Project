e5e59aa466ed21765f85117ca18f7abfc6e81b00
jrose0116 <jrose0116@gmail.com>
2023-12-08 18:18:34 -0500

    Add feed to home

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,17 +1,58 @@
-import React, {useContext} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import {AuthContext} from '../context/AuthContext';
 import '../App.css';
+import axios from "axios"
+import FeedItem from './FeedItem';
+
+const backend = axios.create({baseURL: "http://localhost:3000"})
 
 function Home() {
   const {currentUser} = useContext(AuthContext);
-  // console.log(currentUser);
+  const [loadingFeed, setLoadingFeed] = useState(true)
+  const [loadingItems, setLoadingItems] = useState(true)
+  const [feedData, setFeedData] = useState([])
+  
+  useEffect(()=>{
+    const getData = async () => {
+        const {data} = await backend.get(`/user/${currentUser.uid}/feed`)
+        setFeedData(data)
+        setLoadingFeed(false)
+    }
+    if(currentUser.uid != "") getData()
+  },[])
+
+  console.log(feedData)
+  
+  if(loadingFeed) return (
+    <>
+      <div className='card'>
+        <h2>
+          Hello {currentUser && currentUser.displayName}, {currentUser.uid}, this is the Protected
+          Home page used to show your feed
+        </h2>
+      </div>
+      <div className='card'>Loading...</div>
+    </>
+  )
+
+  const feed = <>
+  {feedData.feed.map((feedItem)=>{
+    return(
+      <FeedItem itemData={feedItem} key={feedItem._id}/>
+    )
+  })}
+  </>
+
   return (
+    <>
     <div className='card'>
       <h2>
         Hello {currentUser && currentUser.displayName}, {currentUser.uid}, this is the Protected
         Home page used to show your feed
       </h2>
     </div>
+      {feed}
+    </>
   );
 }
 
