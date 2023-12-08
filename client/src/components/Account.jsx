597551a3c1adda122637ae77563785305316be15
Isabella-Stone <ibellarose1@gmail.com>
2023-12-08 14:05:31 -0500

    add links to dashboard

--- client/src/components/Account.jsx
+++ client/src/components/Account.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import SignOutButton from './SignOut';
+import {Link} from 'react-router-dom';
 import '../App.css';
 import ChangePassword from './ChangePassword';
 
 function Account() {
   return (
     <div className='card'>
-      <h2>Account Page</h2>
-      <ChangePassword />
+      <h2>Dashboard</h2>
+      <Link>Create Category</Link>
+      <br/>
+      <Link>Add goal</Link>
+      <br/>
+      <Link>Report Expense</Link>
+      <br/>
+      <Link>Edit Profile</Link>
+      <br/>
+      {/* <ChangePassword /> */}
       <SignOutButton />
     </div>
   );
