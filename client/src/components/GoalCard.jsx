1be83818790462a21caaebc80823ca7be2602b69
megxsan <msanford@stevens.edu>
2023-12-09 11:16:06 -0500

    updated the goal card and imports

--- client/src/components/GoalCard.jsx
+++ client/src/components/GoalCard.jsx
@@ -1,6 +1,6 @@
 import { React } from 'react'
 import { Link } from 'react-router-dom';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import {
   Card,
   CardActionArea,
@@ -12,24 +12,59 @@ import { doGetUID } from "../firebase/FirebaseFunctions";
 
 function GoalCard(props){
     const [goal, setGoal] = useState({});
+    const [showExpenses, setShowExpenses] = useState(false)
     
-    async function getGoalInfo(){
+    useEffect( () => {
+      setShowExpenses(false)
+      async function getGoalInfo(){
         let id = doGetUID();
         let data = await axios.get(`http://localhost:3000/userProfile/${id}/${props.id}`)
         setGoal(data.data)
-    }
+        if(data.data.expenses.length != 0){
+          setShowExpenses(true);
+        }
+      }
+      getGoalInfo();
+    }, [])
 
-    getGoalInfo();
     return (
+      <>
         <Grid item sm={4} md={3} lg={2.25} alignItems={"center"} key={props.id}>
-        <Card
-          variant='outlined'
-          sx={{maxWidth: 300, height: 'auto', marginLeft: 'auto', marginRight: 'auto'
-          }}
-        >
-            <h2>{goal.title}</h2>
-        </Card>
-      </Grid>    )
+          <Card
+            variant='outlined'
+            sx={{maxWidth: 300, height: 'auto', marginLeft: 'auto', marginRight: 'auto'
+            }}
+          >
+              <h2>{goal.title}</h2>
+              <h3>{goal.description}</h3>
+              <br/>
+              <h4>Goal Category: {goal.category}</h4>
+              <h4>I want to spend at most ${goal.limit} by {goal.goalDate}</h4>
+
+              {showExpenses && 
+                <>
+                  <h4>Expenses:</h4>
+                  {goal.expenses.map((expense) => {
+                    return(
+                      <>
+                        <h5>{expense.description}</h5>
+                        <br/>
+                        <h6>Spent ${expense.amount} on {expense.date}</h6>
+                      </>
+                    )
+                  })}
+                </>
+              }
+
+              <button onClick={()=>{alert('will add expense for goal here')}}>Add Expense</button>
+              <br/>
+              <br/>
+          </Card>
+        </Grid>    
+        <br/>
+      </>
+
+    )
 }
 
 export default GoalCard
