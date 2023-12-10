f0efd62a11ea41d7f258ca4b6dad2d724ff03fea
jrose0116 <jrose0116@gmail.com>
2023-12-10 02:14:45 -0500

    Add Friends to Navigation

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -23,6 +23,9 @@ const NavigationAuth = () => {
           <NavLink to='/account'>Account</NavLink>
         </li>
         <li>
+          <NavLink to='/friends'>Friends</NavLink>
+        </li>
+        <li>
           <SignOutButton />
         </li>
       </ul>
