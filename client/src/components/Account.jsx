ca821d2ec039995302586d9a2994014bd31ec726
Jacob Rose <jrose0116@gmail.com>
2023-12-15 13:42:51 -0500

    Styling Account.jsx

--- client/src/components/Account.jsx
+++ client/src/components/Account.jsx
@@ -35,23 +35,15 @@ function Account() {
     return (<>Loading...</>)
   }
   else{
+
+    const buttonStyle = {background: "#282c34", width: "20%", color: "white", padding: "10px", textDecoration: "none", fontWeight: '400', borderRadius: "10px"}
     return (
       <>
         
 
         <div className='card'>
-          <h2>Dashboard</h2>
           
-        <div className='mini-nav'>
-            <Link to='./createCategory'>Create Category</Link>
-            <br/>
-            <Link to='./makeGoal'>Add goal</Link>
-            <br/>
-            <Link to='./editProfile'>Edit Profile</Link>
-            <br/>
-            <Link to='./history'>History</Link>
-            <br/>
-        </div>
+          <h2 style={{marginBottom: "10px"}}>Dashboard</h2>
 
         <CardMedia
               component='img'
@@ -62,6 +54,17 @@ function Account() {
         {/* </Card> */}
           {/* <ChangePassword /> */}
           <SignOutButton />
+
+          <div className='mini-nav' style={{display: "flex", gap: "5px", marginTop: "15px"}}>
+              <Link to='./createCategory' style={{...buttonStyle, marginLeft: "auto"}}>Create Category</Link>
+              <br/>
+              <Link to='./makeGoal' style={buttonStyle}>Add goal</Link>
+              <br/>
+              <Link to='./editProfile' style={buttonStyle}>Edit Profile</Link>
+              <br/>
+              <Link to='./history' style={{...buttonStyle, marginRight: "auto"}}>History</Link>
+              <br/>
+          </div>
         </div>
 
         {(goals.length != 0) ? <div className='card'>
