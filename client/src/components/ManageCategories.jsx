12a93493457a2623f852f8229ecde89aff75649b
Jacob Rose <jrose0116@gmail.com>
2023-12-15 17:07:21 -0500

    Styling!

--- client/src/components/ManageCategories.jsx
+++ client/src/components/ManageCategories.jsx
@@ -11,10 +11,10 @@ function ManageCategories({closeForm}) {
   if(tab == 2) return <DeleteCategory closeForm={closeForm}/>
 
   return (
-    <>
-        <button onClick={()=>setTab(1)}>Add category</button>
-        <button onClick={()=>setTab(2)}>Delete category</button>
-    </>
+    <div style={{display: "flex", gap: "10px"}}>
+        <button style={{backgroundColor: "#282c34", color: "white", border: "none", borderRadius: "10px", padding: "10px 20px", width: "40%", marginLeft: "auto"}} onClick={()=>setTab(1)}>Add category</button>
+        <button style={{backgroundColor: "#282c34", color: "white", border: "none", borderRadius: "10px", padding: "10px 20px", width: "40%", marginRight: "auto"}} onClick={()=>setTab(2)}>Delete category</button>
+    </div>
   );
   
 }
