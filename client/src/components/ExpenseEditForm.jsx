389594c9e2e6750871129c0e1e312bd90289dd6d
Jack <jackharrington1117@gmail.com>
2023-12-15 13:25:28 -0500

    Changed error checking.

---
+++ client/src/components/ExpenseEditForm.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react';
+import '../App.css';
+import axios from 'axios';
+import * as firebase from '../firebase/FirebaseFunctions.js';
+import { useThemeProps } from '@mui/material';
+
+function ExpenseEditForm(props) {
+    
+}
+
+export default editExpense
